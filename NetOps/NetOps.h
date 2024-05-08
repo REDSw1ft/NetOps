d0a1b7897ad0aaa2091dfcd497a091455149098d
yegor <esklyarov@baccasoft.ru>
2024-05-08 15:38:15 +0400

    init xcode project

---
+++ NetOps/NetOps.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetOps.
+FOUNDATION_EXPORT double NetOpsVersionNumber;
+
+//! Project version string for NetOps.
+FOUNDATION_EXPORT const unsigned char NetOpsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetOps/PublicHeader.h>
+
+
---
+++ NetOpsTests/NetOpsTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import NetOps
+
+class NetOpsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+}
